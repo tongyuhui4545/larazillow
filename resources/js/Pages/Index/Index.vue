4e64b7ff6ad96fdf001532b1bc87c927400dd32a
Sam <tongyuhui4545@gmail.com>
2025-05-28 14:53:40 +1000

    chore: initial setups

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    vehicle
+    {{ counter }}
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+  const counter = ref(0);
+  setInterval(() => {
+    return counter.value++
+  }, 1000)
+</script>
